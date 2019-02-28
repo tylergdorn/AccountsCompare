2007bf381ec7ce9fddacb6fe2d156da7da38e3b7
Tyler Dorn <tylergdorn@gmail.com>
2019-02-27 15:35:23 -1000

    fixed bug

--- fileProcessing/compare.py
+++ fileProcessing/compare.py
@@ -8,29 +8,29 @@ import fileProcessing.errors as errors
 @errors.ComparisonDecorator
 def compare(alProList: List[classes.Record], QBList: List[classes.Record]):
     """This takes a list of records corresponding to the Al-Pro items and QBList and returns a list of MissingResults and MismatchResults"""
-    expDict = {} 
+    alproDictionary = {} 
     count = 0
     errors = []
     # we load all the alpro errors into a dict
     for item in alProList:
-        expDict[item.invoiceNo] = item
+        alproDictionary[item.invoiceNo] = item
+    print(len(alproDictionary))
     for item in QBList:
         # Then we look at each quickbook item and check if they're there
-        if item.invoiceNo not in expDict:
+        if item.invoiceNo not in alproDictionary:
             pass
             # errors.append(classes.MissingResult(item))
         # We check then if they're there if the amount due matches the expected amount (the alpro amount)
-        elif expDict[item.invoiceNo].totalDue != item.totalDue:
-            # We don't do anything with it now i guess
-            # errors.append(classes.MismatchResult(item, expDict[item.invoiceNo]))
+        elif alproDictionary[item.invoiceNo].totalDue != item.totalDue:
+            errors.append(classes.MismatchResult(item, alproDictionary[item.invoiceNo]))
             # we delete if it doesn't match, because it has been dealt with
-            del expDict[item.invoiceNo]
+            del alproDictionary[item.invoiceNo]
         else:
             # we delete it if it matches
-            del expDict[item.invoiceNo]
+            del alproDictionary[item.invoiceNo]
             count += 1
-        # after we look through all of them, we go through the remaining ones since those are not in the list of quickbook items
-        for item in expDict.values():
-            errors.append(classes.MissingResult(item))
-        # return errors sorted by invoice number
+    # after we look through all of them, we go through the remaining ones since those are not in the list of quickbook items
+    for item in alproDictionary.values():
+        errors.append(classes.MissingResult(item))
+    # return errors sorted by invoice number
     return sorted(errors, key=lambda error: error.record.invoiceNo)
